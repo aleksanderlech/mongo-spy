ae1391ee15d5e6734ed3619cf067d47be6068af5
Aleksander Lech <aleksander@teletronics.ae>
2017-09-19 20:12:49 +0400

    Handle basic rendering of multiple parents stats stages scenario

--- src/main/java/ae/teletronics/mongospy/ui/components/StatsCellFactory.java
+++ src/main/java/ae/teletronics/mongospy/ui/components/StatsCellFactory.java
@@ -2,15 +2,17 @@ package ae.teletronics.mongospy.ui.components;
 
 import ae.teletronics.mongospy.db.model.ExecutionStage;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 public class StatsCellFactory implements Callback<TableColumn<String, ExecutionStage>, TableCell<String, ExecutionStage>> {
@@ -30,17 +32,13 @@ public class StatsCellFactory implements Callback<TableColumn<String, ExecutionS
                 }
 
                 container.getChildren().clear();
-                for (ExecutionStage stage = queryFinalExecutionStage; stage != null; stage = stage.getParentStages().stream().findFirst().orElse(null)) {
-                    container.getChildren().add(buildBox(stage));
-                }
-
-
+                createAndInsertStageBox(container, queryFinalExecutionStage);
                 setGraphic(container);
             }
         };
     }
 
-    private Node buildBox(ExecutionStage executionStage) {
+    private void createAndInsertStageBox(Pane parent, ExecutionStage executionStage) {
         try {
             final AnchorPane template = FXMLLoader.load(getClass().getResource("/fxml/table/StageFragment.fxml"));
 
@@ -51,12 +49,31 @@ public class StatsCellFactory implements Callback<TableColumn<String, ExecutionS
             ((Text) template.lookup("#millis")).setText(executionStage.getMillis() + " ms");
 
             //nReturned
-            Optional.ofNullable(executionStage.getNReturned()).ifPresent(v -> {
-                ((Text) template.lookup("#nReturned")).setText(v.toString());
-            });
+            Optional.ofNullable(executionStage.getNReturned()).ifPresent(v -> ((Text) template.lookup("#nReturned")).setText(v.toString()));
 
 
-            return template;
+            parent.getChildren().add(template);
+
+            //parents
+            List<ExecutionStage> parentStages = executionStage.getParentStages();
+            if(parentStages.size() == 1) {
+                // one parent
+
+                parentStages.stream().findFirst().ifPresent(stage -> createAndInsertStageBox(parent, stage));
+            } else if(parentStages.size() > 1) {
+
+                //multiple parents
+                VBox vBox = new VBox();
+                parent.getChildren().add(vBox);
+
+                parentStages.forEach(stage -> {
+                    HBox hBox = new HBox();
+                    vBox.getChildren().add(hBox);
+                    createAndInsertStageBox(hBox, stage);
+                });
+
+            }
+
 
         } catch (IOException e) {
             throw new RuntimeException("Template load error.", e);
